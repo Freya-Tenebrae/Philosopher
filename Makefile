c6138606c7c72e41e89e2e8cc8134ebe2b8b263d
Celia Maginot <celia.maginot@orange.fr>
2022-02-24 04:53:52 +0100

    clean empty version of philosopher

--- Makefile
+++ Makefile
@@ -6,11 +6,10 @@
 #    By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/14 23:11:43 by cmaginot          #+#    #+#              #
-#    Updated: 2021/11/01 19:48:24 by cmaginot         ###   ########.fr        #
+#    Updated: 2022/02/24 04:47:52 by cmaginot         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 NAME=philo
-NAME_BONUS=philo_bonus
 
 SRCS=$(addprefix ${FOLDER}/, \
 	ft_philo.c)
@@ -29,12 +28,7 @@ RM=rm -f
 
 all: $(NAME)
 
-bonus: $(NAME_BONUS)
-
 $(NAME): $(OBJS)
-	$(CC) $(CFLAGS) -o $@ $^ $(INCLUDES)
-
-$(NAME_BONUS): $(OBJS_BONUS)
 	$(CC) $(CFLAGS) -o $@ $^ $(INCLUDES)
 
 %.o: %.c
---
+++ srcs/philo.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 04:46:01 by cmaginot          #+#    #+#             */
+/*   Updated: 2022/02/24 04:52:23 by cmaginot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 5)
+	{
+		// simulation end when a philosopher die
+	}
+	else if (argc == 6)
+	{
+		// simulation end when a philosopher die or when each philosopher
+			// eat n times
+	}
+	else
+	{
+		// error
+	}
+	(void)argv;
+	return (0);
+}
