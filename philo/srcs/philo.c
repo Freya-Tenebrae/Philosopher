0f89ba8cfa106b8f35ca110d1a1b41669a401a66
Freya-Tenebrae <freya.tenebrae@gmail.com>
2022-02-28 16:16:12 +0100

    all work ... but a little too slow

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 12:42:59 by cmaginot          #+#    #+#             */
-/*   Updated: 2022/02/28 15:23:46 by cmaginot         ###   ########.fr       */
+/*   Updated: 2022/02/28 16:14:42 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,32 @@ static void	philo_sleep(t_philo *philo)
 
 static void	philo_eat(t_philo *philo, t_philo *next_philo)
 {
-	(void)next_philo;
-	pthread_mutex_lock(&philo->fork);
-	message(MESSAGE_FORK, philo);
-	pthread_mutex_lock(&next_philo->fork);
-	message(MESSAGE_FORK, philo);
-	message(MESSAGE_EAT, philo);
-	pthread_mutex_lock(&philo->scene->lock);
-	philo->time_start_last_meal = get_timestamp();
-	philo->number_of_time_eat += 1;
-	pthread_mutex_unlock(&philo->scene->lock);
-	usleep(1000 * philo->scene->time_to_eat);
-	pthread_mutex_unlock(&philo->fork);
-	pthread_mutex_unlock(&next_philo->fork);
+	if (philo->scene->nbr_philo > 1)
+	{
+		pthread_mutex_lock(&philo->fork);
+		message(MESSAGE_FORK, philo);
+		pthread_mutex_lock(&next_philo->fork);
+		message(MESSAGE_FORK, philo);
+		message(MESSAGE_EAT, philo);
+		pthread_mutex_lock(&philo->scene->lock);
+		philo->time_start_last_meal = get_timestamp();
+		philo->number_of_time_eat += 1;
+		pthread_mutex_unlock(&philo->scene->lock);
+		usleep(1000 * philo->scene->time_to_eat);
+		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&next_philo->fork);
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->scene->lock);
+		while (philo->scene->status_scene != STOPPED)
+		{
+			pthread_mutex_unlock(&philo->scene->lock);
+			usleep(1000);
+			pthread_mutex_lock(&philo->scene->lock);
+		}
+		pthread_mutex_unlock(&philo->scene->lock);
+	}
 }
 
 void	*philo_loop(void *arg)
